1bd610657153e3f6764f78bac9eb64aa56ea5f26
Anthony Aviles <avilesanthony0617@gmail.com>
2023-04-01 17:19:46 -0500

    updating git ignore and api base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL = "https://restaurant-res-sys-think.herokuapp.com";
+const API_BASE_URL = "http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
